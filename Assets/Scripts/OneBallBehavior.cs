d1b5e79c2210d65b46e1da3df802117b9a0314f8
Manuel Gurrola <manuelgurrola@outlook.com>
2021-11-16 21:13:12 -0600

    Initial version

---
+++ Assets/Scripts/OneBallBehavior.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OneBallBehavior : MonoBehaviour
+{
+    public float XRotation = 0;
+    public float YRotation = 1;
+    public float ZRotation = 0;
+    public float DegreesPerSecond = 180;
+
+    private static int ballNumberCounter = 0;
+    public int BallNumber;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(3 - Random.value * 6, 3 - Random.value * 6, 3 - Random.value * 6);
+
+        this.BallNumber = ++ballNumberCounter;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 axis = new Vector3(XRotation, YRotation, ZRotation);
+        transform.RotateAround(Vector3.zero, axis, DegreesPerSecond * Time.deltaTime);
+        // Debug.DrawRay(Vector3.zero, axis, Color.yellow);
+    }
+}
